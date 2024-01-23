3d5be771c8028f64147614f532e8741b2a14e2d5
Oleksii Dmytriiev <sysqwerty1984@gmail.com>
2024-01-23 19:23:22 +0200

    add module_10_line_programming

---
+++ module_10_line_programming/lp_simplex.py
@@ -0,0 +1,24 @@
+import pulp
+
+# Ініціалізація моделі
+model = pulp.LpProblem("Maximize-Profit", pulp.LpMaximize)
+
+# Визначення змінних
+A = pulp.LpVariable('A', lowBound=0, cat='Integer')  # Кількість продукту А: A>=0
+B = pulp.LpVariable('B', lowBound=0, upBound=10, cat='Integer')  # Кількість продукту Б: 0<=B<=10
+
+# Функція цілі (Максимізація прибутку)
+model += 50 * A + 40 * B, "Profit"
+
+# Додавання обмежень
+model += 5 * A + 2 * B <= 80  # Обмеження для машини №1
+model += 3 * A + 2 * B <= 40  # Обмеження для машини №2
+
+# Розв'язання моделі
+model.solve()
+
+# Вивід результатів
+print("Виробляти продуктів А:", A.varValue)
+print("Виробляти продуктів Б:", B.varValue)
+# Вартість цільової функції
+print(f"Total profit = {pulp.value(model.objective)}")
